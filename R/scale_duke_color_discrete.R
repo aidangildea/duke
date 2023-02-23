c6f4d184237ad011fceb93885561156e682ff32e
aidangildea <aidan.gildea@duke.edu>
2023-02-22 23:34:05 -0500

    vignette updates, split discrete fill/color

---
+++ R/scale_duke_color_discrete.R
@@ -0,0 +1,22 @@
+#' Apply discrete color scale
+#' Partial code for this function can be attributed to [ggthemes](https://github.com/jrnold/ggthemes/blob/main/R/colorblind.R).
+#'
+#' @param ... Arguments passed on to `discrete_scale`.
+#' @param na.value Color used for NA values
+#' @param guide the name used to create the guide. See [`guides`](https://www.rdocumentation.org/link/guides?package=ggplot2&version=2.1.0) for more info.
+#' @param aesthetics String or vector of strings detailing what aesthetic features this discrete scale can apply to.
+#'
+#' @return a visualization with discrete duke color scale
+#' @export
+#'
+#' @examples
+#' #' # Default example
+#' plot3 <- ggplot2::ggplot(iris, ggplot2::aes(x=Petal.Length, y = Petal.Width, color = Species)) + ggplot2::geom_point()
+#' plot3 + scale_duke_discrete()
+
+scale_duke_color_discrete <- function(..., na.value = "#B5B5B5",
+                                     guide = "legend",
+                                     aesthetics = c("color", "colour")) {
+  ggplot2::discrete_scale(aesthetics = aesthetics, "duke_d_color", duke_pal(),
+                          na.value = na.value, guide = guide, ...)
+}
---
+++ R/scale_duke_fill_discrete.R
@@ -0,0 +1,20 @@
+#' Title
+#'
+#' @param ... Arguments passed on to `discrete_scale`.
+#' @param na.value Color used for NA values
+#' @param guide the name used to create the guide. See [`guides`](https://www.rdocumentation.org/link/guides?package=ggplot2&version=2.1.0) for more info.
+#' @param aesthetics String or vector of strings detailing what aesthetic features this discrete scale can apply to.
+#'
+#' @return a visualization with discrete duke color scale in fill
+#' @export
+#'
+#' @examples
+#' #' # Default example
+#' plot3 <- ggplot2::ggplot(iris, ggplot2::aes(x=Species, fill = Species)) + ggplot2::geom_bar()
+#' plot3 + scale_duke_discrete()
+scale_duke_fill_discrete <- function(..., na.value = "#B5B5B5",
+                                     guide = "legend",
+                                     aesthetics = "fill") {
+  ggplot2::discrete_scale(aesthetics = aesthetics, "duke_d_fill", duke_pal(),
+                          na.value = na.value, guide = guide, ...)
+}
