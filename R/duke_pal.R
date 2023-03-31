8384284958200d90ab408b32a3117876d38d49f1
aidangildea <aidan.gildea@duke.edu>
2023-03-31 16:44:58 -0400

    palette change

--- R/duke_pal.R
+++ R/duke_pal.R
@@ -8,7 +8,7 @@
 #' @examples
 #' duke_pal()
 duke_pal <- function() {
-  values <- c("#012169","#A1B70D", "#C84E00", "#00539B", "#339898", "#E89923",
+  values <- c("#012169", "#C84E00", "#00539B", "#339898", "#A1B70D", "#E89923",
                        "#FFD960", "#262626")
   f <- scales::manual_pal(values)
   attr(f, "max_n") <- length(values)
